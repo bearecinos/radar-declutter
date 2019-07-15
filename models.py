Fixed scans being plotted in wrong order

models.py
@@ -34,6 +34,8 @@ def Henyey_Green(g):
     return (1-g*g)*np.power(1+g,-3.0)
 def Minnaert(theta,k): 
     return np.power(np.cos(theta),2*k-2)
+def Min2(theta):
+    return Minnaert(theta,2) # as can't pass lambda to processor pool
 def raySpecular(theta,n=1):
     return np.power(np.maximum(0,np.cos(theta*2)),n) 
 
@@ -122,25 +124,27 @@ titles = ["Specular n=1"]
 def compare(name,adjusted=False,wave=GaussianDot()):
     returnData = []
     plt.rcParams['axes.formatter.limits'] = [-4,4] # use standard form
-    plt.figure(figsize=(15,10))
+    plt.figure(figsize=(12,8))
     try:
         files = listdir(name)
     except OSError as e:
         fileError(e.filename)
+    files.sort(key=lambda x : int(x[5:])) # assumes 'point' prefix
     heights = []
     
     with open(name+"/"+files[0]+"/x_y_z_elevation","r") as f:
         meta = f.read().split(",")
         x0 = float(meta[0])
         y0 = float(meta[1])
-        cells = int(np.ceil(np.sqrt(len(models))))*110
+    cells = int(np.ceil(np.sqrt(len(models))))*110
     for j in range(len(models)):
         plt.subplot(cells+j+1)
         out = np.full((len(files),_steps),0)
 
         # Bit to parallelize
+        # Note - must pass actual function, not a lambda expression
         p = mp.Pool(mp.cpu_count())
-        data = [(i,(name+"/"+files[i],models[j],wave)) for i in range(len(files))]
+        data = [(i,(name+"/"+files[i],models[j],wave)) for i in range(len(files))] 
         for i,h,ar in p.imap_unordered(worker,data):
             heights.append(h)
             out[i] = ar
@@ -168,6 +172,7 @@ def compare(name,adjusted=False,wave=GaussianDot()):
         #plt.pcolormesh(np.arange(len(files)), ys, draw,cmap="Greys") # alternative cmap is "RdBu_r" where +ve = red
         plt.title(titles[j])
         plt.colorbar()
+        print "Plot complete: "+titles[j]
     plt.show()
     return returnData
 
@@ -180,7 +185,7 @@ def worker(args):
 
 def wiggle(filename,intensityModel=raySpecular,wave=GaussianDot()):
     plt.rcParams['axes.formatter.limits'] = [-4,4] # use standard form
-    plt.figure(figsize=(15,10))
+    plt.figure(figsize=(12,8))
     ys = processSlice(filename,intensityModel=raySpecular,wave=wave)
     xs = np.linspace(0,_MAXDIST*2.0/3e8,_steps)
     plt.plot(xs,ys,color="black")
@@ -194,6 +199,7 @@ def manyWiggle(name,adjusted=False,intensityModel=raySpecular,wave=GaussianDot()
         files = listdir(name)
     except OSError as e:
         fileError(e.filename)
+    files.sort(key=lambda x : int(x[5:])) # assumes "point" prefix
     heights = []
     
     with open(name+"/"+files[0]+"/x_y_z_elevation","r") as f:
@@ -203,8 +209,9 @@ def manyWiggle(name,adjusted=False,intensityModel=raySpecular,wave=GaussianDot()
     cells = int(np.ceil(np.sqrt(len(files))))*110
     
     out = np.full((len(files),_steps),0)
-    for i in progressbar.progressbar(range(len(files))):
+    for i in range(len(files)):
         filename = files[i]
+        #filename = "point"+str(i)
         with open(name+"/"+filename+"/x_y_z_elevation","r") as f:
             meta = f.read().split(",")
             heights.append(float(meta[2]))
@@ -229,7 +236,6 @@ def manyWiggle(name,adjusted=False,intensityModel=raySpecular,wave=GaussianDot()
     plt.ylim((_MAXDIST+highest-lowest)*2.0/3e8,0)
     plt.xlim(-m,m*len(files))
     plt.gca().axes.get_xaxis().set_visible(False)
-    print np.amax(abs(draw))
     plt.show()
     
 def showWave(wave=GaussianDot()):

path.py
@@ -1,17 +1,13 @@
 import stateless
-import progressbar
-from numpy import genfromtxt
+from progressbar import progressbar
+import numpy as np
 import xml.etree.cElementTree as ET
 import utm
 import multiprocessing as mp
 #https://docs.python.org/3/library/xml.etree.elementtree.html#xml.etree.ElementTree.Element
 
 def workerCall(args):
-    global pool
-    if stateless.generateMaps(*args):
-        pool.terminate()
-        return -1
-    return 0
+    return stateless.generateMaps(*args)
     
 def genPath(xs,ys,zs,name,isOffset=True):
     global pool
@@ -28,15 +24,16 @@ def genPath(xs,ys,zs,name,isOffset=True):
     m[[0,-1]] = 0
     m2 = np.roll(m,-1)
     m3 = np.roll(m,1)
-    direction[m] = 180.0/np.pi*(xs[m2]-xs[m3], ys[m2]-ys[m3]))+180.0
-
+    direction[m] = 180.0/np.pi*np.arctan2(xs[m2]-xs[m3], ys[m2]-ys[m3])+180.0
+    steps = len(xs)
     pool = mp.Pool(mp.cpu_count())
     data = [(x,y,name+"/point"+str(i),z,isOffset,angle) for x,y,i,z,angle in
-                            zip(xs,ys,np.arange(_steps),zs,direction)]
+                            zip(xs,ys,np.arange(steps),zs,direction)]
     fail = False
-    for r in progressbar(pool.imap_unordered(workerCall,data),max_value=_steps):
+    for r in progressbar(pool.imap_unordered(workerCall,data),max_value=steps):
         if r == -1:
             fail = True
+            break
     pool.close()
     if fail:
         print "Failed to generate one or more points using stateless.generateMaps"
@@ -58,7 +55,7 @@ def loadFromFile(filename,outName=None):
     try:
         with open(filename,"r") as f:
             isOffset = eval(f.readline())
-            data = genfromtxt(filename,delimiter=",",skip_header=1).swapaxes(0,1)
+            data = np.genfromtxt(filename,delimiter=",",skip_header=1).swapaxes(0,1)
     except IOError:
         print "Error in path.py, could not load file: "+filename
         print "Check filename correct and file in specified format."
@@ -81,17 +78,15 @@ def loadGpx(filename,outName=None):
         else:
             outName = n.text
     lats,lons,zs = [],[],[]
-    track = root.find("trk")
-    for seg in track.findall("trkseg"):
-	for pt in seg.findall("trkpt"):
-	    lats.append(pt.get("lat"))
-	    lons.append(pt.get("lon"))
-	    zs.append(pt.find("ele").text)
+    for pt in root.iter("trkpt"): # recursively searches for points in document order
+        lats.append(pt.get("lat"))
+        lons.append(pt.get("lon"))
+        zs.append(pt.find("ele").text)
     lats = np.array(lats,float)
     lons = np.array(lons,float)
     zs = np.array(zs,float)
     # Convert Lon/lat to x,y for coordinate system
     xs,ys,zoneNum,zoneLet = utm.from_latlon(lats,lons) # says zone 45R?
     # x and y coordinates still appear correct though
-    return gePath(xs,ys,zs,outName,False)
+    return genPath(xs,ys,zs,outName,False)
     

stateless.py
@@ -139,12 +139,13 @@ def generateMaps(pointx,pointy,path,above_ground=100.0,isOffset=True,antennaDir=
     vis = viewshed.viewshed(heightmap,(pointx-cropLeft)/30.0, (pointy-cropLow)/30.0,above_ground,isOffset)
     
     
-    pointCoords = [int(pointx-cropLeft)/30,int(pointy-cropLow)/30]
+    pointCoords = np.array([(pointx-cropLeft)/30.0,(pointy-cropLow)/30.0])
+    groundHeight = viewshed.quadHeight(heightmap,pointCoords[0],_cropHeight-1.0-pointCoords[1])
     if isOffset:
-        elevation = heightmap[_cropHeight-pointCoords[1],pointCoords[0]]+above_ground
+        elevation = groundHeight+above_ground
     else:
         elevation = above_ground
-        above_ground = elevation - heightmap[_cropHeight-pointCoords[1],pointCoords[0]]
+        above_ground = elevation - groundHeight
         isOffset = True
 
     distances = _makeDist2D(vis,pointx,pointy,cropLeft,cropLow)
@@ -169,5 +170,6 @@ def generateMaps(pointx,pointy,path,above_ground=100.0,isOffset=True,antennaDir=
     
 if __name__=="__main__":
     if Setup():
-        return -1
-    print generateMaps(469900.0, 3095000.0,"point1")
+        print "Failed"
+    else:
+        generateMaps(469900.0, 3095000.0,"point1")